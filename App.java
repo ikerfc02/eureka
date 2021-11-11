4eb5b52237be9a7c4ede7220eadb01805548918e
ikerfc <ikerfc02@gmail.com>
2021-11-11 13:10:48 +0100

    Modificado app y readme.

--- App.java
+++ App.java
@@ -1,9 +1,12 @@
 public class App {
   
   public static void main(String[] args) {
+    System.out.println("Iker Fernandez Carpintero");
+    System.out.println("Leon");
     System.out.println("Contenido 2a");
     System.out.println("Contenido 3");
     System.out.println("Contenido 2b");
+    System.out.println("11/11/2021");
   }
   
 }
