0e3923ba978154a7deb78bfe1f0fd7dc3ec625b9
ikerfc <ikerfc02@gmail.com>
2021-11-11 13:17:50 +0100

    Añadido color favorito

--- App.java
+++ App.java
@@ -3,6 +3,7 @@ public class App {
   public static void main(String[] args) {
     System.out.println("Iker Fernandez Carpintero");
     System.out.println("Leon");
+    System.out.println("Purpura");
     System.out.println("Contenido 2a");
     System.out.println("Contenido 3");
     System.out.println("Contenido 2b");
